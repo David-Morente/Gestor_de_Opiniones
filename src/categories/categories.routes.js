7db8e528089bfadbb1874b61216841f471798a8e
David-Morente <davidmorenteg07@gmail.com>
2025-02-21 23:58:27 -0600

    Role de admin agregado

--- src/categories/categories.routes.js
+++ src/categories/categories.routes.js
@@ -1,17 +1,18 @@
 import { Router } from "express"
 import { createCategorieValidator, findCategorieByIdValidator, deleteCategorieValidator, updateCategorieValidator } from "../middlewares/categories-validators.js"
 import { createCategorie, findCategorieById, deleteCategorie, updateCategorie, getCategories} from "./categories.controller.js"
+import { ValidarRol } from "../middlewares/validar-rol.js"
 
 const router = Router()
 
-router.post("/createCategorie", createCategorieValidator, createCategorie)
+router.post("/createCategorie", createCategorieValidator, ValidarRol('ADMIN_ROLE'), createCategorie)
 
 router.get("/", getCategories)
 
-router.get("/findCategorie/:uid", findCategorieByIdValidator, findCategorieById)
+router.get("/findCategorie/:uid", findCategorieByIdValidator, ValidarRol('ADMIN_ROLE'),findCategorieById)
 
-router.delete("/deleteCategorie/:uid", deleteCategorieValidator, deleteCategorie)
+router.delete("/deleteCategorie/:uid", deleteCategorieValidator, ValidarRol('ADMIN_ROLE'), deleteCategorie)
 
-router.put("/updateCategorie/:uid", updateCategorieValidator, updateCategorie)
+router.put("/updateCategorie/:uid", updateCategorieValidator, ValidarRol('ADMIN_ROLE'), updateCategorie)
 
 export default router
